6f0af70474097b3fae96eb21ba74a8e3dc625100
bogdunchik <bogdun0713@gmail.com>
2026-06-24 05:37:20 +0600

    fix db ssl

--- lib/db.ts
+++ lib/db.ts
@@ -10,6 +10,9 @@ const db = mysql.createPool({
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 export async function initDb() {
