453f0591aad281c59734c25f79e2d6682d20b86a
bogdunchik <bogdun0713@gmail.com>
2026-06-24 08:34:17 +0600

    ok

--- lib/db.ts
+++ lib/db.ts
@@ -1,4 +1,4 @@
-import mysql from 'mysql';
+import mysql from 'mysql2/promise';
 if (!process.env.DB_HOST || !process.env.DB_USER || !process.env.DB_PASSWORD || !process.env.DB_NAME) {
   console.error('Отсутствуют переменные окружения для подключения к БД');
 }
